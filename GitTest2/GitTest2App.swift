486f28559adadf7563345be6ba1495e30627dd24
qihan-z <alicqihan0905@gmail.com>
2021-05-13 18:49:32 +0800

    Initial Commit

---
+++ GitTest2/GitTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitTest2App.swift
+//  GitTest2
+//
+//  Created by QiHan Zhao on 2021/5/13.
+//
+
+import SwiftUI
+
+@main
+struct GitTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
